cabeea0f1e12dd782e4cca841e7c2c34fb7af433
TitouanDor <titouan.dorier@outlook.fr>
2025-11-19 08:03:04 +0100

    update main.c

--- main.c
+++ main.c
@@ -8,6 +8,9 @@ int main(void){
     r1 = add_premise(r1, "Moteur");
     r1 = add_premise(r1, "Roue");
     r1 = add_premise(r1, "Volant");
+    r1 = add_premise(r1, "Volant");
+    print_rules(r1);
+    r1->premise = remove_duplicates(r1->premise);
     print_rules(r1);
     r1 = add_conclusion(r1, "roue");
     print_rules(r1);

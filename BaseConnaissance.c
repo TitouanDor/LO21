41986368a1f2c8c1c0b2b68869c17aa76e5afc53
TitouanDor <titouan.dorier@outlook.fr>
2025-11-28 17:04:03 +0100

    toutes fonction et structure liée au base de fais et connaissance déplacer dans baseconnaissance.c et ajout commentaire

--- BaseConnaissance.c
+++ BaseConnaissance.c
@@ -19,6 +19,42 @@ BC add_rules(BC bc, Regle *r){
     return bc;
 }
 
+BC create_example_BC(void) {
+    BC base_connaissance = create_BC();
+    
+    Regle *r2 = new_rule();
+    add_premise(r2, "Pluie");
+    add_premise(r2, "Froid");
+    add_conclusion(r2, "Neige");
+    base_connaissance = add_rules(base_connaissance, r2);
+
+    Regle *r1 = new_rule();
+    add_premise(r1, "Nuages");
+    add_conclusion(r1, "Pluie");
+    base_connaissance = add_rules(base_connaissance, r1);
+
+    Regle *r3 = new_rule();
+    add_premise(r3, "Neige");
+    add_conclusion(r3, "Hiver");
+    base_connaissance = add_rules(base_connaissance, r3);
+
+    Regle *r4 = new_rule();
+    add_premise(r4, "Soleil");
+    add_premise(r4, "Canicule");
+    add_conclusion(r4, "été");
+    base_connaissance = add_rules(base_connaissance, r4);
+
+    return base_connaissance;
+}
+
+BaseFait create_example_BF(void){
+    BaseFait new = NULL;
+    new = add_to_queue(new, "Nuages");
+    new = add_to_queue(new, "Froid");
+    new = add_to_queue(new, "Soleil");
+    return new;
+}
+
 BaseFait MI(BC baseC, BaseFait baseF){
     int modif = 0; //0 si aucune modif n'a été faite sinon 1
     if(!baseF){
@@ -65,4 +101,20 @@ void print_BC(BC bc){
         temp = temp->next;
     }
     printf(" ]\n");
+}
+
+void print_BF(BaseFait bf){
+    if(!bf){
+        printf("La base de faits est vide\n");
+        return;
+    }
+    BaseFait temp = bf; 
+    while(temp){
+        printf("%s", temp->name);
+        if(temp->next){
+            printf(", ");
+        }
+        temp = temp->next;
+    }
+    printf("\n");
 }
